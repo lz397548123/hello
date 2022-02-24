7d4453910d5a809b459873e40c277e1529ef156e
Liang <a397548123@icloud.com>
2022-02-24 18:12:25 +0800

    feat：SayHi现在支持给指定人打招呼啦

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lz397548123/hello/v2
+
+go 1.17
---
+++ hello.go
@@ -0,0 +1,8 @@
+package hello
+
+import "fmt"
+
+// SayHi 向制定人打招呼的函数
+func SayHi(name string) {
+	fmt.Printf("你好%s，我是梁壮。很高兴认识你。\n", name)
+}
